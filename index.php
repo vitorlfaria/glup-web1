ec14cf1eddd1a2542579be307424bee75487a886
Vitor <vitorlacerdafaria7@gmail.com>
2022-08-17 23:26:53 -0300

    Ajustes na tela inicial

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
   require_once "head.php";
   require_once "header.php";
+?>
+	<main class="pagina-principal">
+        <div class="banner">
+            <h1>Avalie e converse sobre os <span>bares e pubs</span> que você ama!</h1>
+            <p>E os que não ama tanto assim.</p>
+        </div>
+        <section class="mais-populares">
+            <div class="titulo">
+                <h2>TOP! Bares mais <span>bem avaliados</span></h2>
+                <hr>
+            </div>
+        </section>
+	</main>
+<?php
   require_once "footer.php";
+?>
