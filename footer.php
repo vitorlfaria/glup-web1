49acdad83bdcedfd602b3bcbe76f4e3065a784e5
Vitor <vitorlacerdafaria7@gmail.com>
2022-08-23 13:59:15 -0300

    Montagem inicial do footer e efeito no hover dos cards populares

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
-    <div>Footer</div>
+    <footer>
+        <img src="./img/glup-logo.svg" alt="Logo Glup" class="logo-footer">
+        <span class="copy-footer">&copy GLUP - 2022 | Todos os direitos reservados</span>
+    </footer>
 </body>
 </html>
