673e4355b2b4516557323dac4d49f76698b51f6c
Vitor <vitorlacerdafaria7@gmail.com>
2022-09-11 13:54:55 -0300

    Adicionado reviews do usuário na página de perfil

--- usuario_editar.php
+++ usuario_editar.php
@@ -114,6 +114,13 @@
         }
     }
 
+    $query = "SELECT nota, avaliacao, nome_bar
+              FROM avaliacoes a
+              INNER JOIN bares b on a.id_bar = b.id_bar
+              WHERE a.id_usuario = '$user_id'
+    ";
+    $resultAvaliacoes = mysqli_query($conn, $query);
+
     disconnect_db($conn);
     require_once "head.php";
     require_once "header.php";
@@ -135,107 +142,146 @@
             </div>
         </div>
         <button class="deletar-usuario">Deletar usuário</button>
-    </div>
-    <form enctype="multipart/form-data" action="<?= $_SERVER['PHP_SELF'] ?>" method="post" class="form form-editar-perfil">
-        <h2>Editar informações do cadastro</h2>
-        <fieldset class="fieldset inline">
-            <label for="foto-perfil" class="input input-file">
-                <i class="fa-solid fa-cloud-arrow-up"></i> Foto de perfil
-                <input type="file"
-                       id="foto-perfil"
-                       name="foto-perfil"
-                       class="input"
-                       hidden>
-            </label>
-            <label for="nome">
-                Nome:
-                <input type="text"
-                       name="nome"
-                       placeholder="Insira seu nome"
-                       value="<?php if(!empty($editar_nome)) echo $editar_nome ?>"
-                       class="input"
-                >
-            </label>
+    </div><!-- sidebar -->
 
-            <label for="sobrenome">
-                Sobrenome:
-                <input type="text"
-                       name="sobrenome"
-                       placeholder="Insira seu sobrenome"
-                       value="<?php if(!empty($editar_sobrenome)) echo $editar_sobrenome ?>"
-                       class="input"
-                >
-            </label>
-        </fieldset>
+    <div class="sidebar">
+        <form enctype="multipart/form-data" action="<?= $_SERVER['PHP_SELF'] ?>" method="post" class="form form-editar-perfil">
+            <h2>Editar informações do cadastro</h2>
+            <fieldset class="fieldset inline">
+                <label for="foto-perfil" class="input input-file">
+                    <i class="fa-solid fa-cloud-arrow-up"></i> Foto de perfil
+                    <input type="file"
+                           id="foto-perfil"
+                           name="foto-perfil"
+                           class="input"
+                           hidden>
+                </label>
+                <label for="nome">
+                    Nome:
+                    <input type="text"
+                           name="nome"
+                           placeholder="Insira seu nome"
+                           value="<?php if(!empty($editar_nome)) echo $editar_nome ?>"
+                           class="input"
+                    >
+                </label>
 
-        <fieldset class="fieldset inline">
-            <label for="login">
-                Usuário:
-                <input type="text"
-                       name="login"
-                       placeholder="Insira seu email"
-                       value="<?php if(!empty($editar_login)) echo $editar_login ?>"
-                       class="input"
-                >
-            </label>
+                <label for="sobrenome">
+                    Sobrenome:
+                    <input type="text"
+                           name="sobrenome"
+                           placeholder="Insira seu sobrenome"
+                           value="<?php if(!empty($editar_sobrenome)) echo $editar_sobrenome ?>"
+                           class="input"
+                    >
+                </label>
+            </fieldset>
 
-            <label for="email">
-                Email:
-                <input type="email"
-                       name="email"
-                       placeholder="Insira seu email"
-                       value="<?php if(!empty($editar_email)) echo $editar_email ?>"
-                       class="input"
-                >
-            </label>
-        </fieldset>
+            <fieldset class="fieldset inline">
+                <label for="login">
+                    Usuário:
+                    <input type="text"
+                           name="login"
+                           placeholder="Insira seu email"
+                           value="<?php if(!empty($editar_login)) echo $editar_login ?>"
+                           class="input"
+                    >
+                </label>
 
-        <fieldset class="fieldset inline">
-            <label for="senha-antiga" class="<?php if (!empty($erro_senha_antiga)) {echo 'tem-erro';} ?>">
-                Senha antiga:
-                <input type="password"
-                       name="senha-antiga"
-                       placeholder="Digite sua senha antiga"
-                       class="input"
-                >
-            </label>
+                <label for="email">
+                    Email:
+                    <input type="email"
+                           name="email"
+                           placeholder="Insira seu email"
+                           value="<?php if(!empty($editar_email)) echo $editar_email ?>"
+                           class="input"
+                    >
+                </label>
+            </fieldset>
 
-            <label for="senha-nova">
-                Senha nova:
-                <input type="password"
-                       name="senha-nova"
-                       placeholder="Crie uma senha nova"
-                       class="input"
-                >
-            </label>
+            <fieldset class="fieldset inline">
+                <label for="senha-antiga" class="<?php if (!empty($erro_senha_antiga)) {echo 'tem-erro';} ?>">
+                    Senha antiga:
+                    <input type="password"
+                           name="senha-antiga"
+                           placeholder="Digite sua senha antiga"
+                           class="input"
+                    >
+                </label>
 
-            <label for="senha-nova-conf" class="<?php if (!empty($erro_senha_conf)) {echo 'tem-erro';} ?>">
-                Confirme a senha nova:
-                <input type="password"
-                       name="senha-nova-conf"
-                       placeholder="Confirmação de senha nova"
-                       class="input"
-                >
-            </label>
-        </fieldset>
-        <?php if (!empty($erro_msg)): ?>
-            <span class="insucesso"><?= $erro_msg ?></span>
-        <?php elseif (!empty($sucesso)): ?>
-            <span class="sucesso"><?= $sucesso ?></span>
-        <?php endif; ?>
-        <button type="submit" class="btn btn-verde btn-scale">SALVAR</button>
-    </form>
-    <div class="modal-deletar">
-        <p class="confirmacao">Tem certeza que deseja apagar sua conta?</p>
-        <p class="aviso">Essa ação não poderá ser desfeita.</p>
-        <div class="btn-container">
-            <form action="deletar_usuario.php" method="POST">
-                <input type="submit" name="deletar" value="Deletar" class="btn btn-verde">
-            </form>
-            <button class="deletar-usuario">Cancelar</button>
+                <label for="senha-nova">
+                    Senha nova:
+                    <input type="password"
+                           name="senha-nova"
+                           placeholder="Crie uma senha nova"
+                           class="input"
+                    >
+                </label>
+
+                <label for="senha-nova-conf" class="<?php if (!empty($erro_senha_conf)) {echo 'tem-erro';} ?>">
+                    Confirme a senha nova:
+                    <input type="password"
+                           name="senha-nova-conf"
+                           placeholder="Confirmação de senha nova"
+                           class="input"
+                    >
+                </label>
+            </fieldset>
+            <?php if (!empty($erro_msg)): ?>
+                <span class="insucesso"><?= $erro_msg ?></span>
+            <?php elseif (!empty($sucesso)): ?>
+                <span class="sucesso"><?= $sucesso ?></span>
+            <?php endif; ?>
+            <button type="submit" class="btn btn-verde btn-scale">SALVAR</button>
+        </form>
+
+        <div class="perfil-avaliacoes-container">
+            <div class="titulo">
+                <h2>Avaliações</h2>
+            </div>
+            <?php if(mysqli_num_rows($resultAvaliacoes) > 0): ?>
+                <?php while($avaliacao = mysqli_fetch_assoc($resultAvaliacoes)): ?>
+                    <div class="avaliacao">
+                        <div class="nome-nota">
+                            <p class="nome-user"><?= $avaliacao['nome_bar'] ?></p>
+                            <div class="estrelas">
+                                <?php
+                                $nota = round($avaliacao['nota']);
+                                ?>
+                                <?php for ($i = 1; $i <= $nota; $i++): ?>
+                                    <i class="fa-solid fa-star estrela-marcada"></i>
+                                <?php endfor; ?>
+                                <?php for ($i = 1; $i <= 5 - $nota; $i++): ?>
+                                    <i class="fa-solid fa-star"></i>
+                                <?php endfor; ?>
+                            </div>
+                            <div class="container-editar" onclick="abrirFormEditarReview(this)">
+                                <span>Editar</span>
+                                <i class="fa-solid fa-pen-to-square editar-review"></i>
+                            </div>
+                        </div>
+                        <?php if($avaliacao['avaliacao']): ?>
+                            <p class="comentario"><?= $avaliacao['avaliacao'] ?></p>
+                        <?php endif; ?>
+                    </div>
+                <?php endwhile; ?>
+            <?php else: ?>
+                <h3 class="nenhum-msg">Você ainda não fez nenhuma avaliação até agora.</h3>
+            <?php endif; ?>
         </div>
     </div>
 </main>
+
+<div class="modal-deletar">
+    <p class="confirmacao">Tem certeza que deseja apagar sua conta?</p>
+    <p class="aviso">Essa ação não poderá ser desfeita.</p>
+    <div class="btn-container">
+        <form action="deletar_usuario.php" method="POST">
+            <input type="submit" name="deletar" value="Deletar" class="btn btn-verde">
+        </form>
+        <button class="deletar-usuario">Cancelar</button>
+    </div>
+</div>
 
 <script>
     const fotoInput = document.querySelector('#foto-perfil')
@@ -244,7 +290,7 @@
         labelFotoInput.classList.add("fundo-verde")
     })
 
-    // Método para mostrar modal ao clicar no botão de deletar usuário
+    // Função para mostrar e esconder modal de confirmação para deletar
     const btnDeletarPerfil = document.querySelectorAll('.deletar-usuario')
     btnDeletarPerfil.forEach(btn => {
         btn.addEventListener('click', () => {
@@ -252,6 +298,11 @@
             modalConfirmacao.classList.toggle('show-modal')
         })
     })
+
+    // Função para mostrar form para editar review
+    function abrirFormEditarReview(nomeBar) {
+
+    }
 </script>
 
 <?php require_once "footer.php"; ?>
